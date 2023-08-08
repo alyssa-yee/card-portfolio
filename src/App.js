ecf684b4b5ace1c7d5d9698b1d4b49fc0f28ae79
alyssa-yee <alyssayee99@gmail.com>
2023-08-08 02:33:15 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [isFlipped, setIsFlipped] = useState(false); //default false
+  const handleCardClick = () => {
+    setIsFlipped(!isFlipped); // not false => setIsFlipped state is set to true
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onClick={handleCardClick}>
+      <div className={`flip-card ${isFlipped ? 'rotate-flip' : ''}`}>
+
+        <div className="flip-card-front">
+        <div className='signature'></div>
+        </div>
+
+        <div className = "flip-card-back">
+          <div className='profile-pic'></div>
+          <div className='name'></div>
+          <div className='links'></div>
+          <div className='paragraph'></div>
+        </div>
+      </div>
     </div>
   );
 }
