3806f117e8823e9e35ca58df45e25a4d1c3a4d73
alyssa-yee <alyssayee99@gmail.com>
2023-08-22 17:35:20 -0700

    e.stoppropogation

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import { useState } from 'react';
 
 function App() {
   const [isFlipped, setIsFlipped] = useState(false); //default false
+
   const handleCardClick = () => {
     setIsFlipped(!isFlipped); // not false => setIsFlipped state is set to true.
+  };
+  
+  const handleButtonClick = (e) => {
+    e.stopPropagation(); // Prevent the click event from bubbling up
   };
 
   return (
@@ -17,8 +22,20 @@ function App() {
 
         <div className="flip-card-back">
           <div className='profile-pic'></div>
-          <div className='name-div'><text id='name'>ALYSSA YEE</text></div>
-          <div className='links'></div>
+
+          <div className='name-div'><span id='name'>ALYSSA YEE</span></div>
+
+          <div className='links'>
+            <div className='info-div'>
+            <span className='info-text'>alyssaqyee@outlook.com</span>
+            <span className='info-text'>(520)-208-7560</span>
+            </div>
+            <div className='buttons-div'>
+            <button className='buttons' onClick={handleButtonClick}>test</button>
+            <button className='buttons' onClick={handleButtonClick}>test</button>
+            </div>
+          </div>
+
           <div className='paragraph'></div>
         </div>
       </div>
